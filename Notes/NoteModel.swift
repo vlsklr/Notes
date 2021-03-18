d53051c2f07775d796d936f7f92a1822d464e8c7
vlsklr <42057101+vlsklr@users.noreply.github.com>
2021-03-18 10:20:37 +0700

    Update NoteModel.swift

--- Notes/NoteModel.swift
+++ Notes/NoteModel.swift
@@ -12,26 +12,13 @@ class Note: Object {
     @objc dynamic var tittle: String = ""
     @objc dynamic var noteText: String?
     @objc dynamic var image: Data?
-   // @objc dynamic let createDate: NSDate
-    //@objc dynamic var editeDate: NSDate
+
     
     convenience init(id: Int, tittle: String, noteText: String?, image: Data?) {
         self.init()
         self.id = id
         self.tittle = tittle
         self.noteText = noteText
-        self.image = image
-        //self.createDate = NSDate()
-        //self.editeDate = NSDate()
-        
+        self.image = image        
     }
-    /*init(id: Int, tittle: String, noteText: String) {
-        //super.init()
-        self.id = id
-        self.tittle = tittle
-        self.noteText = noteText
-       // self.createDate = NSDate()
-        //self.editeDate = NSDate()
-    }
- */
 }
